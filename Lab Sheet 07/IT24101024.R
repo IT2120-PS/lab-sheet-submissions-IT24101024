7b0c3f09cd54e782b2f9b0f630ea838810ef32ab
IT24101024 <it24101024@my.sliit.lk>
2025-09-16 12:10:11 +0530

    Lab_07

---
+++ Lab Sheet 07/IT24101024.R
@@ -0,0 +1,14 @@
+setwd("C:\\Users\\USER\\Desktop\\IT24101024")
+
+#01
+punif(25, min = 0, max = 40) - punif(10, min = 0, max = 40)
+
+#02
+pexp(2, rate = 1/3)
+
+#03
+#i
+1 - pnorm(130, 100, 15)
+
+#ii
+qnorm(0.95, 100, 15)
